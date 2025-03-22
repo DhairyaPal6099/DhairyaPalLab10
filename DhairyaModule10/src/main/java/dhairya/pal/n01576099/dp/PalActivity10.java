7da1176d4e74ca579019dfb2798e8da5a08b2a0d
DhairyaPal6099 <n01576099@humber.ca>
2025-03-21 22:57:55 -0400

    Renamed all object IDs to start with 'dha'. Removed all unnecessary textView definitions in layout xmls

--- DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/PalActivity10.java
+++ DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/PalActivity10.java
@@ -25,7 +25,7 @@ public class PalActivity10 extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = findViewById(R.id.dha_nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -33,7 +33,7 @@ public class PalActivity10 extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.dhaNavView, navController);
     }
 
 }
