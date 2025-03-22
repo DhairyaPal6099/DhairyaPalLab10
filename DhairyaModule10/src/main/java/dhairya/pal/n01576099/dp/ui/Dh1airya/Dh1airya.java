f52e71a8ac5ddf8f7c76e8c297687bbe97bd730b
DhairyaPal6099 <n01576099@humber.ca>
2025-03-22 11:55:04 -0400

    Moved all the hardcoded strings in Dh1airya to strings.xml

--- DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/Dh1airya/Dh1airya.java
+++ DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/Dh1airya/Dh1airya.java
@@ -22,6 +22,7 @@ import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
+import dhairya.pal.n01576099.dp.R;
 import dhairya.pal.n01576099.dp.databinding.FragmentDh1airyaBinding;
 
 public class Dh1airya extends Fragment {
@@ -34,10 +35,9 @@ public class Dh1airya extends Fragment {
         binding = FragmentDh1airyaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        imageUrls.put("Mosasaurus", "https://www.jurassicworlduniverse.com/wordpress/wp-content/uploads/2014/11/mosasaurus3.jpg");
-        imageUrls.put("Salamander", "https://th.bing.com/th/id/OIP.gJxBw1ag8Sjts3jwA-6hPQHaEK?rs=1&pid=ImgDetMain");
-        imageUrls.put("Komodo Dragon", "https://th.bing.com/th/id/OIP.tWwVMYghAk-MK3GthH0odAHaE7?w=284&h=189&c=7&r=0&o=5&dpr=1.3&pid=1.7");
-        imageUrls.put("Pterodactyl", "https://cdn.mos.cms.futurecdn.net/6vA8wWskCkUgffN9dbQhvh-1200-80.jpg");
+        for (int i = 0; i < 4; i++) {
+            imageUrls.put(getResources().getStringArray(R.array.spinner_items)[i], getResources().getStringArray(R.array.spinner_urls)[i]);
+        }
 
         Button buttonDownloadImage = binding.dhaButton;
         ProgressBar progressBar = binding.dhaProgressBar;
@@ -63,7 +63,7 @@ public class Dh1airya extends Fragment {
                         public void onError(Exception e) {
                             new Handler(Looper.getMainLooper()).postDelayed(() -> {
                                 progressBar.setVisibility(View.GONE);
-                                Toast.makeText(getContext(), "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                                Toast.makeText(getContext(), getString(R.string.error) + e.getMessage(), Toast.LENGTH_LONG).show();
                             }, 5000);
                         }
                     });
