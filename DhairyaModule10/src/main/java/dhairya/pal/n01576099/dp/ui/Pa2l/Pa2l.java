c1addd2f16531843e221a213916279549991f1d1
DhairyaPal6099 <n01576099@humber.ca>
2025-03-22 12:31:40 -0400

    Populated the spinner with cities. Created a hashmap of cities and their coordinates.

--- DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/Pa2l/Pa2l.java
+++ DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/Pa2l/Pa2l.java
@@ -10,16 +10,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.util.HashMap;
+
+import dhairya.pal.n01576099.dp.R;
 import dhairya.pal.n01576099.dp.databinding.FragmentPa2lBinding;
 
 public class Pa2l extends Fragment {
     private FragmentPa2lBinding binding;
+    private HashMap cities = new HashMap();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentPa2lBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        //Putting the city values to their corresponding coordinates in the HashMap cities
+        for (int i = 0; i < 5; i++) {
+            cities.put(getResources().getStringArray(R.array.spinner_cities)[i], getResources().getStringArray(R.array.spinner_cities_lat_lon)[i]);
+        }
 
         return root;
     }
