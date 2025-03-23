1228c849d06f3a3d91631d03d99fc94766372330
DhairyaPal6099 <n01576099@humber.ca>
2025-03-22 20:51:50 -0400

    Added the counter logic to AdView and declared dummy adview ID in AndroidManifest.xml

--- DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/D4P/D4P.java
+++ DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/D4P/D4P.java
@@ -10,17 +10,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdView;
+
+import dhairya.pal.n01576099.dp.R;
 import dhairya.pal.n01576099.dp.databinding.FragmentD4pBinding;
 
 public class D4P extends Fragment {
     private FragmentD4pBinding binding;
+    private int counter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentD4pBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.dhaAdView.setOnClickListener(view -> {
+            counter++;
+            Toast.makeText(getContext(), getString(R.string.full_name_space) + counter, Toast.LENGTH_LONG).show();
+        });
 
         return root;
     }
