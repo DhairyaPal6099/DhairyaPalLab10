1437e9789cf4827ee90cc32217aec7949bae2438
DhairyaPal6099 <n01576099@humber.ca>
2025-03-23 00:32:42 -0400

    Added the notification functionality to D4P fragment.

--- DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/D4P/D4P.java
+++ DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/D4P/D4P.java
@@ -7,12 +7,17 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationRequest;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 import android.provider.Settings;
@@ -26,6 +31,7 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
+
 import dhairya.pal.n01576099.dp.R;
 import dhairya.pal.n01576099.dp.databinding.FragmentD4pBinding;
 
@@ -34,6 +40,9 @@ public class D4P extends Fragment {
     private boolean permissionRequestedBefore = false;
     private FragmentD4pBinding binding;
     private ActivityResultLauncher<String> locationPermissionLauncher;
+    private ActivityResultLauncher<String> notificationPermissionLauncher;
+    private NotificationManagerCompat manager;
+    private NotificationCompat.Builder builder;
     private int counter;
     private double latitude;
     private double longitude;
@@ -59,6 +68,21 @@ public class D4P extends Fragment {
                 snackbar.show();
             }
         });
+
+        //Setting up request launcher for notifications permissions
+        notificationPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), o -> {
+            if (o) {
+                showNotification();
+            }
+        });
+
+        //Setting up notification manager and channel
+        NotificationChannelCompat channel = new NotificationChannelCompat.Builder("my_channel_id", NotificationManagerCompat.IMPORTANCE_DEFAULT)
+                .setName("Location Updates")
+                .setDescription("Notifications for location updates")
+                .build();
+        manager = NotificationManagerCompat.from(getContext());
+        manager.createNotificationChannel(channel);
 
         binding.dhaButtonLocation.setOnClickListener(view -> {
             getAndShowLocation();
@@ -87,11 +111,27 @@ public class D4P extends Fragment {
                if (location != null) {
                    latitude = location.getLatitude();
                    longitude = location.getLongitude();
+                   showNotification();
                    Snackbar snackbar = Snackbar.make(getView(), String.format(getString(R.string.latitude_f_longitude_f), latitude, longitude), Snackbar.LENGTH_INDEFINITE);
                    snackbar.setAction(R.string.dismiss, view1 -> snackbar.dismiss());
                    snackbar.show();
                }
            });
        }
+   }
+
+   private void showNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            } else {
+                builder = new NotificationCompat.Builder(getContext(), "my_channel_id")
+                        .setSmallIcon(R.drawable.app_logo)
+                        .setContentTitle("Dhairya Pal Location Determined")
+                        .setContentText(String.format(getString(R.string.latitude_f_longitude_f), latitude, longitude))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                manager.notify(1, builder.build());
+            }
+        }
    }
 }
