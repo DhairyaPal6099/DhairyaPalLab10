a2e60e77dbe81cd0f1ca859ab375203c0404ace5
DhairyaPal6099 <n01576099@humber.ca>
2025-03-22 21:38:05 -0400

    Added the request permissions launcher.

--- DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/D4P/D4P.java
+++ DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/D4P/D4P.java
@@ -2,9 +2,17 @@
 
 package dhairya.pal.n01576099.dp.ui.D4P;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationRequest;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,13 +21,21 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import dhairya.pal.n01576099.dp.R;
 import dhairya.pal.n01576099.dp.databinding.FragmentD4pBinding;
 
 public class D4P extends Fragment {
     private FragmentD4pBinding binding;
+    private ActivityResultLauncher<String> locationPermissionLauncher;
     private int counter;
+    private double latitude;
+    private double longitude;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +48,27 @@ public class D4P extends Fragment {
             Toast.makeText(getContext(), getString(R.string.full_name_space) + counter, Toast.LENGTH_LONG).show();
         });
 
+        //Setting up request launcher for location permissions
+        locationPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean o) {
+                if (o) {
+                   getAndShowLocation();
+                } else {
+                    Snackbar snackbar = Snackbar.make(getView(), "Permission denied", Snackbar.LENGTH_INDEFINITE);
+                    snackbar.setAction("DISMISS", view1 -> snackbar.dismiss());
+                    snackbar.show();
+                }
+            }
+        });
+
+        binding.dhaButtonLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getAndShowLocation();
+            }
+        });
+
         return root;
     }
 
@@ -40,4 +77,24 @@ public class D4P extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+   private void getAndShowLocation() {
+       if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+       } else {
+           FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+           fusedLocationProviderClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY, null).addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+               @Override
+               public void onSuccess(Location location) {
+                   if (location != null) {
+                       latitude = location.getLatitude();
+                       longitude = location.getLongitude();
+                       Snackbar snackbar = Snackbar.make(getView(), String.format("Latitude: %f, Longitude: %f", latitude, longitude), Snackbar.LENGTH_INDEFINITE);
+                       snackbar.setAction("DISMISS", view1 -> snackbar.dismiss());
+                       snackbar.show();
+                   }
+               }
+           });
+       }
+   }
 }
