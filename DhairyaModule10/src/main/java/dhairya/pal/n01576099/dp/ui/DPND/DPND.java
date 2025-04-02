82f6f7a2a944baa859960a30fb4a72c8fbeeddfd
DhairyaPal6099 <n01576099@humber.ca>
2025-04-02 18:33:21 -0400

    Implemented the functionality where the course name edit text changes all the lowercase characters to uppercase and the course description edit text doesn't accept any digits.

--- DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/DPND/DPND.java
+++ DhairyaModule10/src/main/java/dhairya/pal/n01576099/dp/ui/DPND/DPND.java
@@ -6,27 +6,51 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import dhairya.pal.n01576099.dp.R;
+import dhairya.pal.n01576099.dp.databinding.FragmentDpndBinding;
 
 public class DPND extends Fragment {
+    private FragmentDpndBinding binding;
 
     public DPND() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dpnd, container, false);
+        binding = FragmentDpndBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        EditText courseNameEditText = binding.dhaCourseNameEditText;
+        EditText courseDescriptionEditText = binding.dhaCourseDescriptionEditText;
+
+        courseNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String uppercaseEditable = editable.toString().toUpperCase();
+                if (!editable.toString().equals(uppercaseEditable)) {
+                    courseNameEditText.setText(uppercaseEditable);
+                    courseNameEditText.setSelection(uppercaseEditable.length());
+                }
+            }
+        });
+
+        return root;
     }
 }
